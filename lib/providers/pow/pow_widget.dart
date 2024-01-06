8c0c53bd3aba8564fa2a46076c2ee528a7d92e4e
Somoon <dev@moonano.net>
2024-01-06 05:52:10 +0300

    improved pow

---
+++ lib/providers/pow/pow_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PowWidget extends StatefulWidget {
+const PowWidget({Key? key}) : super(key: key);
+
+@override
+_PowWidgetState createState() => _PowWidgetState();
+}
+
+class _PowWidgetState extends State<PowWidget> {
+@override
+Widget build(BuildContext context) {
+return Container();
+}
+}
